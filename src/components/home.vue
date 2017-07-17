e5bbffbb2c865e4698bc536dc4f26c725a39a295
chenxiaoxai <1561992298@qq.com>
2017-07-17 19:33:37 +0800

    veu中使用插件的问题

---
+++ src/components/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>{{msg}}</p>
+        <i class="iconfont icon-zuowei"></i>
+        <button id="d1">click</button>
+    </div>
+</template>
+<script>
+    //import $ from 'jQuery';//会报错
+    console.log($,"输出成功");//可以显示
+
+    export default {
+        name:"home",
+        data(){
+            return {
+                msg:"这是home组件"
+            }
+        }
+    }
+
+    $("#d1").on("click",function(){//调用不成功
+        console.log(11);
+    });
+</script>
+<style lang="">
+    
+</style>
